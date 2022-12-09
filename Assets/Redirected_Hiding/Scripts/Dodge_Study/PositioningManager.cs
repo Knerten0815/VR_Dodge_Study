7eaa7eb2ffed13ce9bf9604865866d9c656f636e
Kevin Zielke <64468215+KevinZielke@users.noreply.github.com>
2022-12-09 20:42:18 +0100

    Positioning and direction checks.

--- Assets/Redirected_Hiding/Scripts/Dodge_Study/PositioningManager.cs
+++ Assets/Redirected_Hiding/Scripts/Dodge_Study/PositioningManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 namespace Dodge_Study
 {
@@ -9,6 +10,14 @@ namespace Dodge_Study
         public Vector2 boundaryCenter;
         public float centerMargin;
         public Transform centerTrans;
+
+        [SerializeField] Camera cam;
+        [SerializeField] GameObject lookDirection;
+
+        private Transform userTrans, lookDirTrans;
+        private InputDevice device;
+        private bool checkPositioning;
+        private bool isPositioned, isDirected;
 
         private static PositioningManager _instance;
         public static PositioningManager Instance { get { return _instance; } }
@@ -32,11 +41,54 @@ namespace Dodge_Study
 #endif
         }
 
+        private void Update()
+        {
+            // this is all just for getting the look at ray
+            Quaternion deviceRot;
+            device.TryGetFeatureValue(CommonUsages.deviceRotation, out deviceRot);
+            lookDirTrans.rotation = deviceRot;
+            Ray r = new Ray(userTrans.position, lookDirTrans.TransformDirection(Vector3.forward));
+
+            /*// test ray
+            Debug.DrawRay(r.origin, r.direction);
+            lookDirection.transform.position = r.origin + r.direction;*/
+
+
+            if (checkPositioning)
+            {
+                float distance = Utilities.FlattenedPos2D(userTrans.position - centerTrans.position).magnitude;
+                if (Mathf.Abs(distance) < 0.1f)
+                    isPositioned = true;
+                else
+                    isPositioned = false;
+
+
+                if (Physics.Raycast(userTrans.position, userTrans.TransformDirection(Vector3.forward)))
+                    isDirected = true;
+                else
+                    isDirected = false;
+            }
+        }
+
         private void Start()
         {
             boundaryPoints = TrackingSpaceGenerator.GetTrackingSpace(out boundaryCenter, out centerMargin);
             centerTrans.localPosition = new Vector3(boundaryCenter.x, 0, boundaryCenter.y);
             Debug.Log("Boundary Center is at " + boundaryCenter.x + ", " + boundaryCenter.y);
+            
+            if (lookDirection == null)
+                lookDirection = new GameObject();
+            lookDirTrans = lookDirection.transform;
+            userTrans = cam.transform;
+
+            var hmdDevices = new List<InputDevice>();
+            InputDevices.GetDevicesWithCharacteristics(InputDeviceCharacteristics.HeadMounted, hmdDevices);
+
+            foreach (var d in hmdDevices)
+            {
+                device = d;
+                Debug.Log("Found device.");
+            }
         }
 
         public void showCenter(bool showCenter)
